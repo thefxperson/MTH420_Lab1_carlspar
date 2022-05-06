2d8e5ae096502e30f4fec34c9757b85703ff84f0
Parker Carlson <carlspar@flip3.engr.oregonstate.edu>
2022-05-06 11:42:45 -0700

    finished lab 6

--- DrazinInverse/drazin.py
+++ DrazinInverse/drazin.py
@@ -1,8 +1,8 @@
 # drazin.py
 """Volume 1: The Drazin Inverse.
-<Name>
-<Class>
-<Date>
+Parker Carlson
+MTH 420
+May 6th, 2022
 """
 
 import numpy as np
@@ -50,7 +50,22 @@ def is_drazin(A, Ad, k):
     Returns:
         (bool) True of Ad is the Drazin inverse of A, False otherwise.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    # condition one
+    if not np.allclose(A @ Ad, Ad @ A):
+        return False
+
+    # condition two
+    Ak = np.linalg.matrix_power(A,k)
+    Ak1 = np.linalg.matrix_power(A,k+1)
+    if not np.allclose(Ak1 @ Ad, Ak):
+        return False
+
+    # condition three
+    if not np.allclose(Ad @ A @ Ad, Ad):
+        return False
+
+    # all conditions satisfied, Ad is Drazin Inv of A
+    return True
 
 
 # Problem 2
@@ -63,7 +78,19 @@ def drazin_inverse(A, tol=1e-4):
     Returns:
        ((n,n) ndarray) The Drazin inverse of A.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    n = A.shape[0]
+    T1,Q1,k1 = la.schur(A,sort=lambda x: abs(x) > tol)
+    T2,Q2,k2 = la.schur(A,sort=lambda x: abs(x) <= tol)
+    U = np.hstack((Q1[:,:k1], Q2[:,:n-k1]))
+    Uinv = np.linalg.inv(U)
+    V = Uinv @ A @ U
+    Z = np.zeros((n,n))
+
+    if k1 != 0:
+      Minv = np.linalg.inv(V[:k1,:k1])
+      Z[:k1,:k1] = Minv
+
+    return U @ Z @ Uinv
 
 
 # Problem 3
@@ -77,7 +104,27 @@ def effective_resistance(A):
         ((n,n) ndarray) The matrix where the ijth entry is the effective
         resistance from node i to node j.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    # find the laplacian of A
+    n = A.shape[0]
+    D = np.sum(A, axis=0) #sum columns to find degree matrix
+    L = (D * np.eye(n)) - A
+
+    R = np.zeros((n,n))
+    for j in range(n):
+        L_j = np.copy(L)
+        L_j[j,:] = np.eye(n)[j,:]
+
+        D = drazin_inverse(L_j)
+        for i in range(n):
+            if i == j:
+              continue
+            if R[i,j] != 0:
+                continue
+
+            R[i,j] = D[i,i]
+            R[j,i] = D[i,i]
+
+    return R
 
 
 # Problems 4 and 5
@@ -110,7 +157,7 @@ class LinkPredictor:
         Raises:
             ValueError: If node is not in the graph.
         """
-        raise NotImplementedError("Problem 5 Incomplete"
+        raise NotImplementedError("Problem 5 Incomplete")
 
 
     def add_link(self, node1, node2):
@@ -125,3 +172,5 @@ class LinkPredictor:
             ValueError: If either node1 or node2 is not in the graph.
         """
         raise NotImplementedError("Problem 5 Incomplete")
+
+
